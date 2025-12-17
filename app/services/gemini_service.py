4f079d2ce0bd1a02d2e2d0e30cf9287734a525aa
SanchitKulkarni1 <sanchit.kulkarni2004@gmail.com>
2025-12-17 22:49:26 +0530

    updated gemini model

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -10,7 +10,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY not found.")
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-pro')
+model = genai.GenerativeModel('gemini-flash-latest')
 
 def get_target_schema() -> str:
     """Generates a JSON string of the BrandContext schema."""
